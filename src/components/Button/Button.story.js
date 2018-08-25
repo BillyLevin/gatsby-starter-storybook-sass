f7a93eeb31eb3fdf47c36bd74d45ed98b6a96ec3
Billy Levin <billylevs@gmail.com>
2018-08-25 20:56:22 +0100

    Add knobs addon to example component

--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
 
 import Button from './index';
 
-storiesOf('Button', module).add('with text', () => <Button>Text only</Button>);
+storiesOf('Button', module).add('with text', () => <Button>{text('Label', 'Text only')}</Button>);
 
-storiesOf('Button', module).add('with emojis', () => <Button>Emojis 👌😎</Button>);
+storiesOf('Button', module).add('with emojis', () => (
+  <Button>{text('Label', 'Emojis 👌😎')}</Button>
+));
